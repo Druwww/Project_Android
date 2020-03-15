e1755ef038c9d6f47f13431f74455bfcbac97149
DRUW <quentin.mulliez@edu.ece.fr>
2020-03-15 19:03:42 +0100

    First version matchs + language

--- My_Curling_App/app/src/main/java/fr/android/quentin/my_curling_app/view_matchs.java
+++ My_Curling_App/app/src/main/java/fr/android/quentin/my_curling_app/view_matchs.java
@@ -3,6 +3,7 @@ package fr.android.quentin.my_curling_app;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -15,6 +16,7 @@ import android.provider.BaseColumns;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -33,9 +35,7 @@ public class view_matchs extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
         myBDD = new managerSQLI.FeedReaderDbHelper(getApplicationContext());
-
     }
 
     @Override
@@ -103,18 +103,60 @@ public class view_matchs extends AppCompatActivity {
                 sortOrder               // The sort order
         );
 
-        LinearLayout layoutMatchs = findViewById(R.id.list_matchs);
+        LinearLayout layoutMatchs = (LinearLayout)findViewById(R.id.list_matchs);
+        layoutMatchs.removeAllViews();
 
         List itemIds = new ArrayList<>();
         while(cursor.moveToNext()) {
             long itemId = cursor.getLong(
                     cursor.getColumnIndexOrThrow(managerSQLI.FeedEntry._ID));
+            String matchName = cursor.getString(
+                    cursor.getColumnIndexOrThrow(managerSQLI.FeedEntry.COLUMN_NAME_MATCH_NAME));
+
+            String matchDate = cursor.getString(
+                    cursor.getColumnIndexOrThrow(managerSQLI.FeedEntry.COLUMN_NAME_MATCH_DATE));
+
+            String matchTime = cursor.getString(
+                    cursor.getColumnIndexOrThrow(managerSQLI.FeedEntry.COLUMN_NAME_MATCH_TIME));
+
+            String finalTextMatch = "Name : " + matchName + "\nDate : " + matchDate + "\nHeure : " + matchTime;
+
+            String matchStatus = cursor.getString(
+                    cursor.getColumnIndexOrThrow(managerSQLI.FeedEntry.COLUMN_NAME_MATCH_STATUS));
+
+            if(matchStatus.equals("1")){
+                finalTextMatch += "\nResultat : Victoire";
+            }else if(matchStatus.equals("2")){
+                finalTextMatch += "\nResultat : Nul";
+            }else{
+                finalTextMatch += "\nResultat : Defaite";
+            }
+
+            String matchPhoto = cursor.getString(
+                    cursor.getColumnIndexOrThrow(managerSQLI.FeedEntry.COLUMN_NAME_MATCH_PICTURE));
+
+            //finalTextMatch += "\nPhoto : " + matchPhoto + "\n\n\n";
+
+            ImageView my_image = new ImageView(this);
+            my_image.setImageURI(Uri.parse(matchPhoto));
 
             TextView newScore = new TextView(this);
-            newScore.setText("ID Trouvé : " + itemId);
+            newScore.setText(finalTextMatch);
             layoutMatchs.addView(newScore);
+            layoutMatchs.addView(my_image);
+            my_image.getLayoutParams().height = 500;
+            my_image.getLayoutParams().width = 500;
+            //my_image.requestLayout();
         }
         cursor.close();
     }
 
+    public void erraseDataBase(View view) {
+
+        SQLiteDatabase db = myBDD.getReadableDatabase();
+
+        int deletedRows = db.delete(managerSQLI.FeedEntry.TABLE_NAME, null, null);
+        launchDisplayMatchs(view);
+
+    }
 }
